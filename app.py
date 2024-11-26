621a9bce662138e3ca2363b704c8e85b76fe29ac
ZEUS-MUSIC <aradhyamusicbots@gmail.com>
2024-11-27 01:27:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return "Radhe Radhe"
+@app.route('/')
+def hello_world():
+    return 'Greymatters'
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run()
